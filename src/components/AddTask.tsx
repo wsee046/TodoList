9b24d7643fba4e7bcd960ee4a1886a2e9243dd97
wsee046 <wsee046@aucklanduni.ac.nz>
2022-12-02 09:51:59 +1300

    Completed the add task component

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,24 +1,54 @@
 import styled, {css} from "styled-components"
 
 const AddTask = () => {
-    const AddTaskCard = styled.div`
-    background: #383636;
-    color: white;
+
+    const Form = styled.form`
+        background: #404040;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 4%;
+        gap: 10%;
     `
+
+    const Field = styled.div`
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    `
+
+    const SubmitButton = styled.input`
+        padding: 1.5% 3% 1.5% 3%;
+        border-radius: 15px;
+        background: orange;
+        color: white;    
+    `
+
+    const TextEntry = styled.input`
+        border-radius: 10px;
+        height: 1.6em;
+        padding-left: 5%;
+        width: 100%;
+    `
+
+    const Label = styled.label`
+        text-align: left;
+        padding-left: 5%;
+    `
+
     return (
-        <AddTaskCard>
-        <form>
-            <div>
-                <label>Name</label>
-                <input type='text'/>
-            </div>
-            <div>
-                <label>Description</label>
-                <input type='text'/>
-            </div>
-            <input type='submit' value='Add Task'/>
-        </form>
-        </AddTaskCard>
+        <Form>
+            <Field>
+                <Label htmlFor="name">Name</Label>
+                <TextEntry id="name" type='text'/>
+            </Field>
+            <Field>
+                <Label htmlFor="description">Description</Label>
+                <TextEntry id="description" type='text'/>
+            </Field>
+            <SubmitButton type='submit' value='Add Todo'/>
+        </Form>
     )
 }
 
