5b691a0b8830abb737f9289457997b32bcaadc80
lia-arroyo <liayzabel@gmail.com>
2022-12-02 09:47:08 +1300

    added styled components to the task card

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,19 +1,67 @@
+import styled from "styled-components";
+
 function TaskCard() {
+  // constants for testing
     const taskName = "Test 1";
     const taskDescription = "Test 2";
-    
+    const taskStatus = false; // true is complete
+
+    const TaskCardContainer = styled.div`
+      display: flex;
+      align-items: baseline;
+      background-color: dimgrey;
+      padding: 2%;
+      border: solid 1px black;
+
+      ${taskStatus && `
+        color: dark-brown;
+        text-decoration: line-through;
+      `}
+
+    `
+
+    const TaskTitle = styled.h1`
+      color: orange;
+      margin-bottom: 0;
+    `
+
+    const TaskDescription = styled.p`
+      color: white;
+    `
+
+    const TaskInfo = styled.div`
+      text-align: left;
+      flex: 3;
+    `
+
+    const Buttons = styled.div`
+      text-decoration: none !important; 
+      flex: 1;
+      display: flex;
+      justify-content: space-between;
+    `
+
+    const Button = styled.button`
+      background-color: white;
+      padding: 0.5em 2em;
+      border-radius: 1.5em;
+      cursor: pointer;
+      color: ${props => props.color ? props.color : "white"};
+      border: solid 1px ${props => props.color ? props.color : "white"};
+    `
+
   return (
-    <div>
-        <div>
-            <h1>{taskName}</h1>
-            <p>{taskDescription}</p>
-        </div>
-        <div>
-            <button>Edit</button>
-            <button>Complete</button>
-            <button>Delete</button>
-        </div>
-    </div>
+    <TaskCardContainer>
+        <TaskInfo>
+            <TaskTitle>{taskName}</TaskTitle>
+            <TaskDescription>{taskDescription}</TaskDescription>
+        </TaskInfo>
+        <Buttons>
+            <Button color="green">Complete</Button>
+            <Button color="blue">Edit</Button>
+            <Button color="red">Delete</Button>
+        </Buttons>
+    </TaskCardContainer>
   )
 }
 
